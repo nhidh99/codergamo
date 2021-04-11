ae8a6b60f1b546d626c312f6bca542565aeb7a06
Nhi Dinh <nhidh99@gmail.com>
2021-04-11 19:45:57 +0700

    Reformat project 003

--- 003-undo-redo-pane/script.js
+++ 003-undo-redo-pane/script.js
@@ -2,48 +2,48 @@ const undoStack = [];
 const redoStack = [];
 
 function logState() {
-	console.log("--- Current State ---");
-	console.log("pane color = " + getPaneColor());
-	console.log("undo stack = " + undoStack);
-	console.log("redo stack = " + redoStack);
+    console.log("--- Current State ---");
+    console.log("pane color = " + getPaneColor());
+    console.log("undo stack = " + undoStack);
+    console.log("redo stack = " + redoStack);
 }
 
 function changeColor() {
-	const oldColor = getPaneColor();
-	const newColor = event.target.style.backgroundColor;
+    const oldColor = getPaneColor();
+    const newColor = event.target.style.backgroundColor;
 
-	if (oldColor !== newColor) {
-		undoStack.push(oldColor);
-		redoStack.length = 0;
-		setPaneColor(newColor);
-	}
+    if (oldColor !== newColor) {
+        undoStack.push(oldColor);
+        redoStack.length = 0;
+        setPaneColor(newColor);
+    }
 }
 
 function undoColor() {
-	if (undoStack.length !== 0) {
-		const oldColor = getPaneColor();
-		const newColor = undoStack.pop();
-		redoStack.push(oldColor);
-		setPaneColor(newColor);
-	}
+    if (undoStack.length !== 0) {
+        const oldColor = getPaneColor();
+        const newColor = undoStack.pop();
+        redoStack.push(oldColor);
+        setPaneColor(newColor);
+    }
 }
 
 function redoColor() {
-	if (redoStack.length !== 0) {
-		const oldColor = getPaneColor();
-		const newColor = redoStack.pop();
-		undoStack.push(oldColor);
-		setPaneColor(newColor);
-	}
+    if (redoStack.length !== 0) {
+        const oldColor = getPaneColor();
+        const newColor = redoStack.pop();
+        undoStack.push(oldColor);
+        setPaneColor(newColor);
+    }
 }
 
 function getPaneColor() {
-	const pane = document.getElementById("pane");
-	return pane.style.backgroundColor;
+    const pane = document.getElementById("pane");
+    return pane.style.backgroundColor;
 }
 
 function setPaneColor(color) {
-	const pane = document.getElementById("pane");
-	pane.style.backgroundColor = color;
-	logState();
+    const pane = document.getElementById("pane");
+    pane.style.backgroundColor = color;
+    logState();
 }
